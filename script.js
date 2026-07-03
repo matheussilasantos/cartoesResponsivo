0360207a768c5ac497a47777f9f52773e2e59a76
Santos-LadrãoDeBurro <matheus.santos.silva06@escola.pr.gov.br>
2026-07-03 12:47:14 +0000

    deu certo!!!!!!!!!!!!!!!!!!!! até que enfim

---
+++ script.js
@@ -0,0 +1,27 @@
+const botao1 = document.getElementById("botao1")
+const botao2 = document.getElementById("botao2")
+const botao3 = document.getElementById("botao3")
+const botao4 = document.getElementById("botao4")
+const botao5 = document.getElementById("botao5")
+
+const caixa = document.getElementById("caixa-cartoes");
+
+botao1.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
+
+botao2.addEventListener('click', () => {
+    alert("bbbbbbbbbbbbbbb");
+});
+
+botao3.addEventListener('click', () => {
+    alert("cccccccccccccccc");
+});
+
+botao4.addEventListener('click', () => {
+    alert("ddddddddddddddddd");
+});
+
+botao5.addEventListener('click', () => {
+    alert("eeeeeeeeeeeeeeeeeee");
+});
